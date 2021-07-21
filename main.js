ff3d6d6bb5d176454103a1ce9c60407d1138efc3
trungle1403 <64635586+trungle1403@users.noreply.github.com>
2021-07-21 13:33:32 +0700

    update v6

--- main.js
+++ main.js
@@ -9,3 +9,25 @@ window.addEventListener('scroll',function(){
     var header = document.querySelector('.header');
     header.classList.toggle('sticky',window.scrollY>0);
 })
+
+
+//menu active
+// $(document).ready(function() {
+    $(window).scroll(function() {
+        var scrollDistance = $(window).scrollTop();
+        //Show/hide menu on scroll
+        // if (scrollDistance >= 850) {
+        //     $('.header').fadeIn();
+        // } else {
+        //     $('.header').fadeOut();
+        // }
+        // Assign active class to nav links while scolling
+        $('.page-section').each(function(i) {
+                if ($(this).position().top <= scrollDistance) {
+                    $('.menu a.active').removeClass('active');
+                    $('.menu a').eq(i).addClass('active');
+                }
+        });
+    }).scroll();
+// });
+
